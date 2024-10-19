66521c285a21ccdcfcd53824f75a8afa8e78c2f8
xjt <969211735@qq.com>
2024-10-19 13:57:43 +0800

    drm first
    drm first

--- src/main.c
+++ src/main.c
@@ -2,27 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <fcntl.h>
-#include <sys/mman.h>
-#include <wayland-client.h>
-#include <time.h>
+#include <unistd.h>
 #include <errno.h>
+#include <sys/mman.h>
+#include <time.h>
+#include <xf86drm.h>
+#include <xf86drmMode.h>
+// #include <drm_fourcc.h>
+#include "/home/xjt/_Workspace_/VOC/System/RV1126_RV1109_LINUX_SDK_V2.2.5.1_20230530/buildroot/output/rockchip_rv1126_rv1109/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/drm/drm_fourcc.h"
 
-#define WINDOW_WIDTH 1920
-#define WINDOW_HEIGHT 1080
 #define RECT_WIDTH 100
 #define RECT_HEIGHT 100
-#define FPS 60
 #define NUM_RECTS 8
-
-struct wl_display *display = NULL;
-struct wl_registry *registry = NULL;
-struct wl_compositor *compositor = NULL;
-struct wl_surface *surface = NULL;
-struct wl_shell *shell = NULL;
-struct wl_shell_surface *shell_surface = NULL;
-struct wl_shm *shm = NULL;
+#define FPS 60
 
 struct Rectangle {
     int x, y;
@@ -32,72 +25,37 @@ struct Rectangle {
 
 struct Rectangle rects[NUM_RECTS];
 
-void *shm_data = NULL;
-struct wl_buffer *buffer = NULL;
+int fd;
+drmModeConnector *connector;
+drmModeEncoder *encoder;
+drmModeModeInfo mode;
+uint32_t crtc_id;
+struct drm_mode_create_dumb create_dumb = {0};
+struct drm_mode_map_dumb map_dumb = {0};
+uint32_t fb_id;
+uint8_t *map;
+int width, height;        // Actual display dimensions
+int buffer_width, buffer_height;  // Buffer dimensions (aligned)
+int stride;               // Bytes per row in the buffer
 
-static void registry_handler(void *data, struct wl_registry *registry, uint32_t id,
-                             const char *interface, uint32_t version) {
-    if (strcmp(interface, "wl_compositor") == 0) {
-        compositor = wl_registry_bind(registry, id, &wl_compositor_interface, 1);
-    } else if (strcmp(interface, "wl_shell") == 0) {
-        shell = wl_registry_bind(registry, id, &wl_shell_interface, 1);
-    } else if (strcmp(interface, "wl_shm") == 0) {
-        shm = wl_registry_bind(registry, id, &wl_shm_interface, 1);
-    }
-}
+void initialize_rects() {
+    uint32_t colors[NUM_RECTS] = {
+        0xFFFF0000,  // Red
+        0xFF00FF00,  // Green
+        0xFF0000FF,  // Blue
+        0xFFFFFF00,  // Yellow
+        0xFFFF00FF,  // Purple
+        0xFF00FFFF,  // Cyan
+        0xFF000000,  // Black
+        0xFFFFFFFF   // White
+    };
 
-static const struct wl_registry_listener registry_listener = {
-    registry_handler,
-    NULL
-};
-
-static struct wl_buffer* create_buffer() {
-    int stride = WINDOW_WIDTH * 4;
-    int size = stride * WINDOW_HEIGHT;
-
-    int fd = memfd_create("buffer", 0);
-    if (fd < 0) {
-        perror("Failed to create memfd");
-        exit(1);
-    }
-
-    if (ftruncate(fd, size) < 0) {
-        perror("Failed to set size of memfd");
-        close(fd);
-        exit(1);
-    }
-
-    shm_data = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    if (shm_data == MAP_FAILED) {
-        perror("mmap failed");
-        close(fd);
-        exit(1);
-    }
-
-    struct wl_shm_pool *pool = wl_shm_create_pool(shm, fd, size);
-    struct wl_buffer *buffer = wl_shm_pool_create_buffer(pool, 0, WINDOW_WIDTH, WINDOW_HEIGHT, stride, WL_SHM_FORMAT_ARGB8888);
-
-    wl_shm_pool_destroy(pool);
-    close(fd);
-
-    return buffer;
-}
-
-void draw_rects() {
-    uint32_t *pixels = (uint32_t *)shm_data;
-    
-    // Clear the entire buffer with transparent color
-    memset(pixels, 0, WINDOW_WIDTH * WINDOW_HEIGHT * 4);
-    
-    // Draw all rectangles
     for (int i = 0; i < NUM_RECTS; i++) {
-        for (int y = rects[i].y; y < rects[i].y + RECT_HEIGHT; y++) {
-            for (int x = rects[i].x; x < rects[i].x + RECT_WIDTH; x++) {
-                if (x >= 0 && x < WINDOW_WIDTH && y >= 0 && y < WINDOW_HEIGHT) {
-                    pixels[y * WINDOW_WIDTH + x] = rects[i].color;
-                }
-            }
-        }
+        rects[i].x = rand() % (width - RECT_WIDTH);
+        rects[i].y = rand() % (height - RECT_HEIGHT);
+        rects[i].velo_x = (rand() % 10) + 5;
+        rects[i].velo_y = (rand() % 10) + 5;
+        rects[i].color = colors[i];
     }
 }
 
@@ -106,73 +64,101 @@ void update_rect_positions() {
         rects[i].x += rects[i].velo_x;
         rects[i].y += rects[i].velo_y;
 
-        // Collision detection
-        if (rects[i].x <= 0 || rects[i].x + RECT_WIDTH >= WINDOW_WIDTH) {
+        if (rects[i].x <= 0 || rects[i].x + RECT_WIDTH >= width) {
             rects[i].velo_x = -rects[i].velo_x;
         }
-        if (rects[i].y <= 0 || rects[i].y + RECT_HEIGHT >= WINDOW_HEIGHT) {
+        if (rects[i].y <= 0 || rects[i].y + RECT_HEIGHT >= height) {
             rects[i].velo_y = -rects[i].velo_y;
         }
     }
 }
 
-void initialize_rects() {
-    uint32_t colors[NUM_RECTS] = {
-        0x80FF0000,  // Red
-        0x8000FF00,  // Green
-        0x800000FF,  // Blue
-        0x80FFFF00,  // Yellow
-        0x80FF00FF,  // Purple
-        0x8000FFFF,  // Cyan
-        0x80000000,  // Black
-        0x80FFFFFF   // White
-    };
-
+void draw_rects() {
+    uint32_t *pixels = (uint32_t *)map;
+    
+    // Clear the entire buffer
+    for (int y = 0; y < buffer_height; y++) {
+        for (int x = 0; x < buffer_width; x++) {
+            pixels[y * (stride / 4) + x] = 0;
+        }
+    }
+    
+    // Draw all rectangles
     for (int i = 0; i < NUM_RECTS; i++) {
-        rects[i].x = rand() % (WINDOW_WIDTH - RECT_WIDTH);
-        rects[i].y = rand() % (WINDOW_HEIGHT - RECT_HEIGHT);
-        rects[i].velo_x = (rand() % 10) + 5;
-        rects[i].velo_y = (rand() % 10) + 5;
-        rects[i].color = colors[i];
+        for (int y = rects[i].y; y < rects[i].y + RECT_HEIGHT; y++) {
+            for (int x = rects[i].x; x < rects[i].x + RECT_WIDTH; x++) {
+                if (x >= 0 && x < width && y >= 0 && y < height) {
+                    pixels[y * (stride / 4) + x] = rects[i].color;
+                }
+            }
+        }
     }
 }
 
-int main(int argc, char **argv) {
-    srand(time(NULL));  // Initialize random seed
+int main() {
+    srand(time(NULL));
 
-    display = wl_display_connect(NULL);
-    if (display == NULL) {
-        fprintf(stderr, "Can't connect to display\n");
-        exit(1);
-    }
-    printf("Connected to display\n");
-
-    registry = wl_display_get_registry(display);
-    wl_registry_add_listener(registry, &registry_listener, NULL);
-
-    wl_display_dispatch(display);
-    wl_display_roundtrip(display);
-
-    if (compositor == NULL || shell == NULL || shm == NULL) {
-        fprintf(stderr, "Can't find all required globals\n");
-        exit(1);
+    fd = open("/dev/dri/card0", O_RDWR);
+    if (fd < 0) {
+        perror("open");
+        return 1;
     }
 
-    surface = wl_compositor_create_surface(compositor);
-    if (surface == NULL) {
-        fprintf(stderr, "Can't create surface\n");
-        exit(1);
+    drmModeRes *resources = drmModeGetResources(fd);
+    if (!resources) {
+        perror("drmModeGetResources");
+        return 1;
     }
 
-    shell_surface = wl_shell_get_shell_surface(shell, surface);
-    if (shell_surface == NULL) {
-        fprintf(stderr, "Can't create shell surface\n");
-        exit(1);
+    for (int i = 0; i < resources->count_connectors; i++) {
+        connector = drmModeGetConnector(fd, resources->connectors[i]);
+        if (connector->connection == DRM_MODE_CONNECTED) {
+            break;
+        }
+        drmModeFreeConnector(connector);
     }
 
-    wl_shell_surface_set_fullscreen(shell_surface, WL_SHELL_SURFACE_FULLSCREEN_METHOD_DEFAULT, 0, NULL);
+    if (!connector) {
+        fprintf(stderr, "No connected connector found\n");
+        return 1;
+    }
 
-    buffer = create_buffer();
+    mode = connector->modes[0];
+    width = mode.hdisplay;
+    height = mode.vdisplay;
+    buffer_width = (width + 15) & ~15;  // Align to 16 pixels
+    buffer_height = height;
+
+    for (int i = 0; i < resources->count_encoders; i++) {
+        encoder = drmModeGetEncoder(fd, resources->encoders[i]);
+        if (encoder->encoder_id == connector->encoder_id) {
+            break;
+        }
+        drmModeFreeEncoder(encoder);
+    }
+
+    if (!encoder) {
+        fprintf(stderr, "No encoder found\n");
+        return 1;
+    }
+
+    crtc_id = encoder->crtc_id;
+
+    create_dumb.width = buffer_width;
+    create_dumb.height = buffer_height;
+    create_dumb.bpp = 32;
+    drmIoctl(fd, DRM_IOCTL_MODE_CREATE_DUMB, &create_dumb);
+
+    stride = create_dumb.pitch;
+
+    drmModeAddFB(fd, buffer_width, buffer_height, 24, 32, stride, create_dumb.handle, &fb_id);
+
+    map_dumb.handle = create_dumb.handle;
+    drmIoctl(fd, DRM_IOCTL_MODE_MAP_DUMB, &map_dumb);
+
+    map = mmap(0, create_dumb.size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, map_dumb.offset);
+
+    drmModeSetCrtc(fd, crtc_id, fb_id, 0, 0, &connector->connector_id, 1, &mode);
 
     initialize_rects();
 
@@ -185,12 +171,7 @@ int main(int argc, char **argv) {
         update_rect_positions();
         draw_rects();
 
-        wl_surface_attach(surface, buffer, 0, 0);
-        wl_surface_damage(surface, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
-        wl_surface_commit(surface);
-
-        wl_display_dispatch_pending(display);
-        wl_display_flush(display);
+        drmModePageFlip(fd, crtc_id, fb_id, DRM_MODE_PAGE_FLIP_EVENT, NULL);
 
         clock_gettime(CLOCK_MONOTONIC, &end);
         long elapsed = (end.tv_sec - start.tv_sec) * 1000000000 + (end.tv_nsec - start.tv_nsec);
@@ -202,15 +183,15 @@ int main(int argc, char **argv) {
         }
     }
 
-    wl_buffer_destroy(buffer);
-    wl_shell_surface_destroy(shell_surface);
-    wl_surface_destroy(surface);
-    wl_shell_destroy(shell);
-    wl_compositor_destroy(compositor);
-    wl_registry_destroy(registry);
-    wl_display_disconnect(display);
-
-    printf("Disconnected from display\n");
+    // Clean up (this part is never reached in this example)
+    drmModeRmFB(fd, fb_id);
+    munmap(map, create_dumb.size);
+    struct drm_mode_destroy_dumb destroy_dumb = { .handle = create_dumb.handle };
+    drmIoctl(fd, DRM_IOCTL_MODE_DESTROY_DUMB, &destroy_dumb);
+    drmModeFreeEncoder(encoder);
+    drmModeFreeConnector(connector);
+    drmModeFreeResources(resources);
+    close(fd);
 
     return 0;
 }
